c7947c592a445402ecfb20da31d9142624b7d597
WZTLEI <w.lei.lightningbolt@gmail.com>
2018-08-05 23:09:23 -0400

    Change app theme; Make full screen

--- app/src/main/java/com/wztlei/tanktrouble/MainActivity.java
+++ app/src/main/java/com/wztlei/tanktrouble/MainActivity.java
@@ -1,7 +1,10 @@
 package com.wztlei.tanktrouble;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatDialog;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
 }
